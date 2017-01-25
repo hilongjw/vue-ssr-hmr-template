d974b70b3b290f4b4ced570edec5180790867f35
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2017-01-25 15:45:12 +0100

    Update webpack.base.js
    update to use *-loader name

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -4,35 +4,35 @@ const webpackHotMiddlewareConfig = 'webpack-hot-middleware/client?path=/__webpac
 const getEntries = require('./getEntries')
 
 module.exports = {
-    context: path.resolve(__dirname, '../'),
-    output: {
-        path: path.resolve(__dirname, '../public'),
-        publicPath: '/',
-        filename: 'client/[name].js',
-        chunkFilename: 'client/[name].js'
-    },
-    resolve: {
-        extensions: ['.js', '.vue']
-    },
-    module: {
-        loaders: [{
-            test: /\.vue$/,
-            loader: 'vue'
-        }, {
-            test: /\.js$/,
-            loader: 'babel',
-            exclude: /node_modules/
-        }, {
-            test: /\.(png|jpg|gif|svg|ttf|woff|eot)$/,
-            loader: 'file',
-            query: {
-                name: 'file/[name].[ext]'
-            }
-        }]
-    },
-    plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ],
+  context: path.resolve(__dirname, '../'),
+  output: {
+    path: path.resolve(__dirname, '../public'),
+    publicPath: '/',
+    filename: 'client/[name].js',
+    chunkFilename: 'client/[name].js'
+  },
+  resolve: {
+    extensions: ['.js', '.vue']
+  },
+  module: {
+    loaders: [{
+      test: /\.vue$/,
+      loader: 'vue-loader'
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.(png|jpg|gif|svg|ttf|woff|eot)$/,
+      loader: 'file-loader',
+      query: {
+        name: 'file/[name].[ext]'
+      }
+    }]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 }
